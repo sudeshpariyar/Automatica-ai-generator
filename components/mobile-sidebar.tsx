b611bce996574a01a67a959208d1599d1deab3be
sudeshpariyar <sudesh.bullet@gmail.com>
2024-04-29 13:24:55 +1000

    implemented the dashboard

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
+  //fix hydration error
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
